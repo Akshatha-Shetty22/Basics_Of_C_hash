4547593a31b6c9b0ca87e547ce07caeaa4c8eeb4
Akshatha Shetty <akshathashettykk@gmail.com>
2022-03-31 09:57:18 +0530

    variables and constants project added

---
+++ Variables_demo/Variablesdemo/ConstDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablesdemo
+{
+    //This program demonstatrates how to use constants
+    class ConstDemo
+    {
+        static void Main()
+        {
+            //Must be assigned at the time of declaration
+            const double PI = 3.14;
+            int r = 5;
+            double aoc = (2 * r * PI);
+            Console.WriteLine($"Area of circle={aoc}");
+            
+        }
+
+    }
+}
---
+++ Variables_demo/Variablesdemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablesdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 100;
+            sbyte sb= -100;//signedbyte-can hold negative as well a positive values
+            short s = 1233;
+            int i = 12345;
+            uint ui= 123;//Can hold only positive number
+            long l = 4322344;
+            ulong u= 1223444; //holds only positive number;
+            double d = 345.45;
+            string str = "welcome";
+            char c = 'A';
+            bool flag = true;
+            Console.WriteLine($"byte={ b}\nshort={ s}\nint={i}\nlong={l}\ndouble={d}\nstring={str}\nchar={c}\nbool={flag}");
+            //object keyword can hold any datatype-It is the base type of all types in .Net
+            //All types are derived from System.Object
+            object intObj = 34;
+            object charobj = 'C';
+            object boolObj = false;
+            object strObj = "welcome";
+            Console.WriteLine($"IntObj={intObj}\ncharObj={charobj}\nboolObj={boolObj}\nstrObj={strObj}\n");
+            Console.ReadLine();
+        }
+    }
+}
